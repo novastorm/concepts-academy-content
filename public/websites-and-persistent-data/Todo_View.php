5dee0e98f9dce906bf25b42364f7904194b7563b
Adland Lee <adland[at]4mfd[dot]com>
2013-12-27 13:49:44 -0800

    Update Websites and Persistent Data Tutorial
    - UI Completed

--- public/websites-and-persistent-data/Todo_View.php
+++ public/websites-and-persistent-data/Todo_View.php
@@ -14,41 +14,65 @@ class Todo_View {
 	public function index ()
 	{
 		$record_list = $this->_model->index();
-		$output = json_encode($record_list);
+		// $output = json_encode($record_list);
+		$output = "";
+		$output .= <<<EOF
+<h2>Todo List <a href="responder.php/todo/create"><button>New</button></a></h2>
+EOF;
+		$output .= "<table>\n";
+
+		foreach ($record_list as $record) {
+			$todo_id = $record['todo_id'];
+			$task = $record['task'];
+
+			$output .= <<<EOF
+<tr>
+	<td><a href="responder.php/todo/show/$todo_id">$task</a></td>
+	<td><a href="responder.php/todo/show/$todo_id/edit">edit</a></td>
+	<td><a href="responder.php/todo/destroy/$todo_id">destroy</a></td>
+</tr>
+EOF;
+		}
+
+		$output .= "\n</table>\n";
 		return $output;
 	}
 
 	public function show ($id)
 	{
 		$record = $this->_model->show($id);
-		$output = json_encode($record);
+
+		$todo_id = $record['todo_id'];
+		$task = $record['task'];
+
+		$output = <<<EOF
+<dl>
+	<dt>ID:</dt>
+	<dd>$todo_id</dd>
+	<dt>Task:</dt>
+	<dd>$task</dd>
+</dl>
+EOF;
 		return $output;
 	}
 
 	public function store ($param)
 	{
-		$record = $this->_model->store($param['task']);
-		$output = json_encode($record);
-		return $output;
+		return $this->_model->store($param['task']);
 	}
 
 	public function update ($id, $param)
 	{
-		$record = $this->_model->update($id, $param['task']);
-		$output = json_encode($record);
-		return $output;
+		return $this->_model->update($id, $param['task']);
 	}
 
 	public function destroy ($id)
 	{
-		$record = $this->_model->destroy($id);
-		$output = json_encode($record);
-		return $output;
+		return $this->_model->destroy($id);
 	}
 
 	public function create ()
 	{
-		$output = "Create View";
 		$output = <<< EOF
 <form action="responder.php/todo/store">
 	<label for="input-task">Task</label>
@@ -61,7 +85,18 @@ EOF;
 
 	public function edit ($id)
 	{
-		$output = "$id: Edit View";
+		$record = $this->_model->show($id);
+
+		$todo_id = $record['todo_id'];
+		$task = $record['task'];
+
+		$output = <<< EOF
+<form action="responder.php/todo/update/$todo_id">
+	<label for="input-task">Task</label>
+	<input type="text" name="input-task" value="$task" placeholder="Task">
+	<button type="submit">Update Task</button>
+</form>
+EOF;
 		return $output;
 	}
 }
